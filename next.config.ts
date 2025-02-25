89ec58d530e357c96f37e21ea4d0426ee2fcf4d4
Corey Hill <corey.business.987@outlook.com>
2025-02-24 20:45:01 -0600

    set up changes in preperation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // @ts-ignore: appDir is a valid experimental flag in our Next.js version
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
